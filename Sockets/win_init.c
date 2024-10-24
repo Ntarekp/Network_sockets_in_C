2dbc24a256b1b3617954257bdf4b10757f2dfb7e
Ntarekp <kpntare@gmail.com>
2024-10-24 16:58:01 +0200

    We can know loop through this linked list and print information for each adapter address

--- Sockets/win_init.c
+++ Sockets/win_init.c
@@ -21,8 +21,6 @@ int main() {
         return 1;
     }
 
-    //Allocate memory to adapters and request adapters addresses from windows using GetAdapterAdresses() function
-
     DWORD asize = 20000;
     PIP_ADAPTER_ADDRESSES adapters = (PIP_ADAPTER_ADDRESSES)malloc(asize);
 
@@ -32,11 +30,9 @@ int main() {
         return -1;
     }
 
-    // Get network adapter information
     int r = GetAdaptersAddresses(AF_UNSPEC, GAA_FLAG_INCLUDE_PREFIX, 0, adapters, &asize);
 
     if (r == ERROR_BUFFER_OVERFLOW) {
-        // Reallocate memory if buffer is too small
         printf("GetAdaptersAddress wants %ld bytes.\n", asize);
         free(adapters);
         adapters = (PIP_ADAPTER_ADDRESSES)malloc(asize);
@@ -45,23 +41,35 @@ int main() {
             WSACleanup();
             return -1;
         }
-    } else if (r == ERROR_SUCCESS) {
-        // Success, process the adapters here
-        while (adapters) {
-            // Loop through the adapter list and print information
-            // Example:
-            // printf("Adapter: %s\n", adapters->FriendlyName);
-            adapters = adapters->Next;
-        }
-    } else {
-        // Handle error
+    } else if (r != ERROR_SUCCESS) {
         printf("Error from GetAdaptersAddress: %d\n", r);
         free(adapters);
         WSACleanup();
         return -1;
     }
 
-    // Cleanup
+    PIP_ADAPTER_ADDRESSES adapter = adapters;  // Initialize adapter here
+
+    while (adapter) {
+        printf("\nAdapter name: %S\n", adapter->FriendlyName);
+        PIP_ADAPTER_UNICAST_ADDRESS address = adapter->FirstUnicastAddress;
+
+        while (address) {
+            printf("\t%s",
+                   address->Address.lpSockaddr->sa_family == AF_INET ?
+                   "IPv4" : "IPv6");
+            char ap[100];
+            getnameinfo(address->Address.lpSockaddr,
+                        address->Address.iSockaddrLength,
+                        ap, sizeof(ap), 0, 0, NI_NUMERICHOST);  // Add missing comma and arguments
+            printf("\t%s\n", ap);
+            address = address->Next;
+        }
+
+        adapter = adapter->Next;  // Correct the loop control
+    }
+
+    free(adapters);
     WSACleanup();
     printf("Ok.\n");
     return 0;
